e61a170ea303d8d28f875e12d78f42157dcf1e4d
flora <flora.parmentier@gmail.com>
2018-07-31 20:08:58 +0200

    Upcoming matches

--- config/config.exs
+++ config/config.exs
@@ -20,6 +20,9 @@ use Mix.Config
 #
 #     config :logger, level: :info
 #
+config :panda,
+  api_key: "YOUR_API_KEY"
+
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
--- lib/panda.ex
+++ lib/panda.ex
@@ -4,15 +4,28 @@ defmodule Panda do
   """
 
   @doc """
-  Hello world.
+  Upcoming matches
+  Retrieve the 5 next upcoming matches from the Pandascore API
 
   ## Examples
 
-      iex> Panda.hello
-      :world
+      iex> PandascoreTechTest.upcoming_matches
+      [
+        %{"begin_at" => "2018-08-01T08:00:00Z", "id" => 49634, "name" => "BBQ-vs-GEN"},
+        %{"begin_at" => "2018-08-01T09:00:00Z", "id" => 50149, "name" => "CR vs VG"},
+        %{"begin_at" => "2018-08-01T09:00:00Z", "id" => 49780, "name" => "VG-vs-TOP"},
+        %{"begin_at" => "2018-08-01T11:00:00Z", "id" => 49705, "name" => "JDG-vs-EDG"},
+        %{"begin_at" => "2018-08-01T11:00:00Z", "id" => 49659, "name" => "SKT-vs-AFS"}
+      ]
 
   """
-  def hello do
-    :world
+  def upcoming_matches do
+    response = HTTPotion.get "https://api.pandascore.co/matches/upcoming.json?token=#{Application.fetch_env!(:panda, :api_key)}"
+    {:ok, all_matches} = Poison.decode(response.body)
+    full_matches = Enum.take(all_matches, 5)
+    for match <- full_matches do
+      %{"begin_at" => match["begin_at"], "id" => match["id"], "name" => match["name"]}
+    end
   end
+
 end
