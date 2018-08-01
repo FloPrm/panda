e2263a61ae4ce0e13494e334a5b3c2a80ee34a8c
flora <flora.parmentier@gmail.com>
2018-08-01 07:03:00 +0200

    Tests

--- test/panda_test.exs
+++ test/panda_test.exs
@@ -2,7 +2,13 @@ defmodule PandaTest do
   use ExUnit.Case
   doctest Panda
 
-  test "greets the world" do
-    assert Panda.hello() == :world
+  test "Upcoming matches" do
+    assert length(Panda.upcoming_matches()) == 5
+  end
+
+  test "Basic Odds" do
+    match_id = Enum.random(Panda.upcoming_matches)["id"]
+    assert Map.values(Panda.odds_for_match(match_id))
+      |> Enum.reduce(0, fn x, acc -> acc + x end) == 100
   end
 end
